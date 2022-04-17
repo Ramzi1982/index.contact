9b4330cb70e1ce5f5f776f18022dbe2bb9976eb5
Ramzi1982 <96336444+Ramzi1982@users.noreply.github.com>
2022-04-17 18:42:45 +0200

    Add files via upload

---
+++ exemple modal.html/script.js
@@ -0,0 +1,67 @@
+function sendEmail(){
+
+    var name = document.getElementById("name").value;
+    var sujet = document.getElementById("sujet").value;
+    var mail = document.getElementById("email").value;
+    var re = /\S+@\S+\.\S+/;
+    
+        if (name != null && sujet != null && mail.match(re)) {
+         
+              Email.send({
+                Host : "nextwebplus.fr",
+                Username : " support@nextwebplus.fr",
+                Password : "Kk345$xm",
+                To : document.getElementById("email").value ,
+                From : 'noreply@nextwebplus.fr' ,
+                Subject : "New Contact Form Enquiry",
+                Body : "Name: " + document.getElementById("name").value
+                    + "<br> Email:" + document.getElementById("email").value
+                    + "<br> phone:" + document.getElementById("phone").value
+                    + "<br> sujet:" + document.getElementById("sujet").value
+                    + "<br> message:"+ document.getElementById("message").value
+            }).then(
+              
+            )
+    
+                    
+        }else{
+         
+         
+        }
+    }
+    
+    //get modal element
+    var modal = document.getElementById('reponseModal');
+    
+    //get open modal button
+    var modalBtn = document.getElementById('submitButton');
+    
+    //get close button
+    var closeBtn = document.getElementsByClassName('closeBtn')[0];
+    
+    //Listen for open Click
+    modalBtn.addEventListener('click',openModal);
+    
+    //Listen for close Click
+    closeBtn.addEventListener('click',closeModal);
+    
+    //Listen for outside click
+    window.addEventListener('click',outsideClick);
+    
+    //function to open modal
+    function openModal(){
+        modal.style.display ='block';
+    }
+    
+    //function to close modal
+    function closeModal(){
+        modal.style.display ='none';
+    }
+    
+    //function to close modal if outside click
+    function outsideClick(e){
+        if(e.target==modal)
+        modal.style.display ='none';
+    }
+    
+    
